2c2868d03297f5e89c737b732569db4f014c6321
Joaozin777 <128042962+Joaozin777@users.noreply.github.com>
2024-10-23 08:57:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const ctx = document.getElementById("myChart").getContext("2dc")
+const meuGrafico =new Chart (grafico,{
+    type:"pie",
+    date: {
+         labels:[
+            "Instragram",
+            "Whatsapp"
+            "Facebook",
+            "WeChat"
+            "TikTok",
+            "DouYin"
+            "Twitter",
+            "Telegram",
+            "FB Messenger"
+            "LINE"
+         ]
+}
